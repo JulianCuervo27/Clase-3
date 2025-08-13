441f7fc4bd897a585f7695cdc4be7d8b6373f0c4
Your Name <you@example.com>
2025-08-13 16:55:59 -0500

    Main: Estructura de datos

---
+++ EstructuraEstaticas.java
@@ -0,0 +1,10 @@
+public class EstructuraEstaticas {
+    public static void main(String[] args) {
+        int a=10;
+        int b=11;
+        int suma =a+b;
+        int resta = a-b;
+        System.out.println("La suma de a+b es: "+suma);
+        System.out.println("La resta de a-b es: "+resta);
+    }
+}
---
+++ EstructuraEstaticas.py
@@ -0,0 +1,6 @@
+a=10
+b=11
+suma=a+b
+resta=a-b
+print("La suma es: "+suma)
+print("La resta es: "+resta)
