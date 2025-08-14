1e8870ce1d3b3804a80d0997c34ad4b22b0c58d0
Your Name <you@example.com>
2025-08-14 17:07:48 -0500

    Agrego programas completos en Java y Python

--- EstructuraEstaticas.java
+++ EstructuraEstaticas.java
@@ -4,7 +4,24 @@ public class EstructuraEstaticas {
         int b=11;
         int suma =a+b;
         int resta = a-b;
+        int multi =a*b;
+        float divi =a/b;
         System.out.println("La suma de a+b es: "+suma);
         System.out.println("La resta de a-b es: "+resta);
+        System.out.println("La multiplicacion es: "+multi);
+        System.out.println("la division es: "+divi);
+        Character A1='H';
+        Character B1='O';
+        Character C1='L';
+        Character D1='A';
+        A1.toString();
+        B1.toString();
+        C1.toString();
+        D1.toString();
+        String suma1 = ""+A1+B1+C1+D1;
+        System.out.println(suma1);
+        boolean t=true;
+        boolean f=false;
+        System.out.println(t+" "+f);
     }
 }
--- EstructuraEstaticas.py
+++ EstructuraEstaticas.py
@@ -1,6 +1,23 @@
-a=10
-b=11
+a = 10
+b = 11
 suma=a+b
-resta=a-b
-print("La suma es: "+suma)
-print("La resta es: "+resta)
+resta = a - b
+multi = a * b
+divi = a / b
+
+print("La suma de a+b es:", suma)
+print("La resta de a-b es:", resta)
+print("La multiplicacion es:", multi)
+print("La division es:", divi)
+
+A1 = 'H'
+B1 = 'O'
+C1 = 'L'
+D1 = 'A'
+
+suma1 = A1 + B1 + C1 + D1
+print(suma1)
+
+t = True
+f = False
+print(t, f)
